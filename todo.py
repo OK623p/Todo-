db3c539645f49f7a6f15a561c9d7dec1e63ce0fd
OK623p <ikarudexi@gmail.com>
2025-05-23 21:14:57 +0900

    Update todo.py
    タスク削除
    優先度をプルダウン(低.中,高)OR 直接入力でできるように
    タスクラインで進捗(×△〇)を選べるように

--- todo.py
+++ todo.py
@@ -20,7 +20,6 @@ def load():
 
 #新規追加
 def add():
-    #データ処理
     task = task_entry.get()
     deadline = deadline_entry.get()
     priority = priority_entry.get()
@@ -32,7 +31,8 @@ def add():
     else:
         messagebox.showinfo("エラー","入力項目不足")
         return
-    update() 
+    
+    update()
     #入力クリア
     task_entry.delete(0,END) 
     deadline_entry.delete(0,END)
@@ -40,21 +40,29 @@ def add():
 
 #完了処理    
 def complete(i):
-    Label(text="済",justify=LEFT,relief=SOLID,width=8,height=2).grid(row=i+1,column=4,sticky="ns")
+    try:
+        del task_list[i]
+        update()
+    except IndexError:
+        messagebox.showinfo("エラー","該当項目なし")
     return
+
 #更新対応
 def update():
-    #入力欄とヘッダー残して全部消す
+    #入力欄とヘッダーと(ボタン)残して全部消す
     for widget in root.grid_slaves():
         info = widget.grid_info()
         if info["row"] not in (0,99) and info["column"] != 0:
             widget.destroy()
+            
     for i,task in enumerate(task_list):
-        
         Label(root,text=task["task"],justify=LEFT,relief=SOLID,width=110,height=2).grid(row=i+1,column=1,sticky="ns")
         Label(root,text=task["deadline"],justify=LEFT,relief=SOLID,width=20,height=2).grid(row=i+1,column=2,sticky="ns")
         Label(root,text=task["priority"],justify=LEFT,relief=SOLID,width=7,height=2).grid(row=i+1,column=3,sticky="ns")
-        Label(root,text="済" if task["completed"] else "未",justify=LEFT,relief=SOLID,width=8,height=2).grid(row=i+1,column=4,sticky="ns")
+        progress_box = ttk.Combobox(root,values = ["×","△","〇"],state ="readonly",width = 4)
+        progress_box.set("×")
+        progress_box.grid(row=i+1, column=4, sticky="ew")
+        #Label(root,text="済" if task["completed"] else "未",justify=LEFT,relief=SOLID,width=8,height=2).grid(row=i+1,column=4,sticky="ns")
     
     with open("task.json","w",encoding="utf-8") as f :
         json.dump(task_list,f,ensure_ascii = False,indent=2)
@@ -70,10 +78,8 @@ t_label_4 = Label(root,text="進捗",justify="center",height=2,relief="solid")
 label = Label(root,text="新規",justify="center",height=2,relief="solid")
 task_entry = Entry(root,font=("明朝体",22),relief="solid",width=50,borderwidth=2)
 deadline_entry = Entry(root,font=("明朝体",22),relief="solid",width=8,borderwidth=2)
-priority_entry = Entry(root,font=("明朝体",22),relief="solid",width=3,borderwidth=2)
+priority_entry = ttk.Combobox(root,values = ["低","中","高"],state = "normal",width = 2,font=("明朝体", 22))
 task_button = Button(root,text = "確定",width=7,height=2,command=add,relief="solid")
-
-
 
 #レイアウト
 for i in range(15):
